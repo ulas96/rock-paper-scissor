e1c26ac7d81ab33f0280315a8495e7ae9efd6255
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-04 01:10:45 +0300

    Update Dashboard.jsx

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -5,19 +5,38 @@ export default function Dashboard({state,account}) {
     const [_account] = account
 
     const [wins, setWins] = useState([]);
+    const [loses,setLoses] = useState([]);
+    const [deuces, setDeuces] = useState([]);
 
     const getPlayerWins = async () => {
         const _wins = await state.contract.getPlayerWins(_account);
         setWins(_wins);
+    } 
+
+    const getPlayerLoses = async () => {
+        const _loses = await state.contract.getPlayerLoses(_account);
+        setLoses(_loses);
     }
+
+const getPlayerDeuces = async () => {
+    const _deuces =  await state.contract.getPlayerDeuces(_account);
+    setDeuces(_deuces);
+}
+
 
     useEffect(() => {
         getPlayerWins();
+        getPlayerLoses();
+        getPlayerDeuces();
     })
 
     return (
         <>
-            {`Number of wins: ${wins.length}`}
+            
+            
+            <p>{`Wins: ${wins.length}`}</p>
+            <p>{`Loses: ${loses.length}`}</p>
+            <p>{`Deuces: ${deuces.length}`}</p>
         </>
     );
 }
