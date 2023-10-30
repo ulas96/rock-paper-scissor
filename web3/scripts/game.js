03772df6c2cc697563a5585fd061f3d20c3add7f
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-30 11:14:38 +0300

    RPS contract update

--- web3/scripts/game.js
+++ web3/scripts/game.js
@@ -1,13 +1,13 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
-const gameAddr = "0xAa84c17C94E8242122200f2532725bC45b572694";
+const gameAddr = "0xC4E531D1a73Cd53d85229BA29C66140D585A1f92" // previous "0xAa84c17C94E8242122200f2532725bC45b572694"
 const contractName = "RPS";
 
 async function main() {
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-  const result = await game.getPendingGame(1);
+  const result = await game.getGames();
   console.log(result);
 }
 
