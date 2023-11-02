9edeb26587e37ef68a316a2aa6515e9d65eb0a51
ulas96 <y.ulas.yildiz@gmail.com>
2023-11-02 08:38:06 +0300

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     goerli: {
       url: process.env.URL,
-      accounts: [`${process.env.PRIVATE_KEY}`]
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
